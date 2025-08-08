3dc0c2530942433eba6ce1b28882f378b604981c
pilot2254 <109202734+pilot2254@users.noreply.github.com>
2025-08-08 18:17:44 +0200

    Termcolor (didnt work anyway

---
+++ termcolor/termcolor.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <termcolor.hpp>
+
+int main() {
+    std::cout << termcolor::red << "Hello "
+              << termcolor::green << "World"
+              << termcolor::reset << std::endl;
+}
