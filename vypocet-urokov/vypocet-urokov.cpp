ee577f79b7ea839bfcfc3c86a9725ec6a0147cb9
pilot2254 <109202734+pilot2254@users.noreply.github.com>
2025-09-30 11:43:06 +0200

    Create vypocet-urokov.cpp

---
+++ vypocet-urokov/vypocet-urokov.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+/*
+Priklad:
+
+Pán Novák vložil na sporiaci účet, ktorý je úročený ročnou úrokovou sadzbou 3 %, sumu 2 000 €. Úroky sú pripisované vždy na konci roka.
+Vypočítajte, akú celkovú sumu bude mať pán Novák na účte po 3 rokoch.
+*/
+
+int main(){
+
+        float num = 2000;
+        float percent = 3;
+        int years = 3;
+
+        for (int i = 0; i < years; i++){
+                num = num / 100 * (100 + percent);
+                std::cout << "Year " << i + 1 << ": " << num << '\n';
+        }
+
+        return 0;
+}
