cf0d34d524d8e2e4efc620a22ed6394a020a5c4a
pilot2254 <109202734+pilot2254@users.noreply.github.com>
2025-12-16 14:21:23 +0100

    create spinner animation

---
+++ spinner.cpp/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+int main() {
+    const char frames[] = {'|', '/', '-', '\\'};
+    int i = 0;
+
+    while (true) {
+        std::cout << "\r" << frames[i % 4] << std::flush;
+        i++;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+}
