9b6013d83329bf77d22aed29552d4b3d56f6f380
Michal Flaška <109202734+pilot2254@users.noreply.github.com>
2026-01-29 15:12:42 +0100

    complete problem 19 (still taking a break from problem 17)

---
+++ practice-tests/19/main.cpp
@@ -0,0 +1,50 @@
+// Write a function that removes all occurrences of a specific value from a vector.
+
+// Examples:
+// - {1, 2, 3, 2, 4} remove 2 → {1, 3, 4}
+// - {5, 5, 5, 5} remove 5 → {}
+// - {1, 2, 3} remove 9 → {1, 2, 3}
+
+// Function: std::vector<int> removeValue(std::vector<int> nums, int val)
+
+// Hints:
+// - Create a new result vector
+// - Loop through original vector
+// - Only add elements that aren't equal to val
+
+#include <iostream>
+#include <vector>
+
+void printVector(std::vector<int> v){ // copypasted this from problem 13, 14, 15
+        std::cout << "{";
+        for(int i = 0; i < v.size(); i++){
+                std::cout << v[i];
+                if(i < v.size() - 1){
+                        std::cout << ", ";
+                }
+        }
+        std::cout << "}";
+}
+
+std::vector<int> removeValue(std::vector<int> nums, int val){
+
+        std::vector<int> result = {};
+
+        for(int i = 0; i < nums.size(); i++){
+                if(nums[i] != val){
+                        result.push_back(nums[i]);
+                }
+        }
+
+        return result;
+}
+
+int main(){
+
+        std::vector<int> v = {1, 2, 3, 2, 4};
+        int n = 2;
+
+        printVector(removeValue(v, n));
+
+        return 0;
+}
