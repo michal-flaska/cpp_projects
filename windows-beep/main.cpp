f0627b8607602bca5d76263880aeaf6def61c8f7
Michal Flaška <109202734+pilot2254@users.noreply.github.com>
2026-02-02 14:08:31 +0100

    "borrow" arduino's pitches.h

--- windows-beep/main.cpp
+++ windows-beep/main.cpp
@@ -1,25 +1,21 @@
-#include <windows.h>
 #include <iostream>
+#include <windows.h>
 
-#define C4 262
-#define D4 294
-#define E4 330
-#define F4 349
-#define G4 392
-#define A4 440
-#define B4 494
-#define C5 523
+#include "notes.h"
+
+// just a little note to myself:
+// Beep() maxes out at around 32767 Hz,
+// and has a lower limit around 37 Hz
 
 int main(){
 
-        //happy birthday
-
-        Beep(G4, 200);
-        Beep(G4, 200);
-        Beep(A4, 400);
-        Beep(G4, 400);
-        Beep(C5, 400);
-        Beep(B4, 800);
+        // happy birthday
+        // Beep(G4, 200);
+        // Beep(G4, 200);
+        // Beep(A4, 400);
+        // Beep(G4, 400);
+        // Beep(C5, 400);
+        // Beep(B4, 800);
 
         return 0;
 }
