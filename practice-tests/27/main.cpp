0e21b1481446c018cf3072654773a8cf17550184
Michal Flaška <109202734+pilot2254@users.noreply.github.com>
2026-02-05 09:50:07 +0100

    solve problem 27

--- practice-tests/27/main.cpp
+++ practice-tests/27/main.cpp
@@ -14,3 +14,28 @@
 // - A prime number is only divisible by 1 and itself
 // - Check if any number from 2 to n-1 divides n evenly (n % i == 0)
 // - If you find one that does, it's not prime
+
+#include <iostream>
+
+bool isPrime(int n){
+
+        if(n == 1){
+                return false;
+        }
+
+        for (int i = 2; i < n; i++){
+                if (n % i == 0){
+                        return false;
+                }
+        }
+        return true;
+}
+
+int main(){
+
+        int n = 10;
+
+        std::cout << isPrime(n);
+
+        return 0;
+}
